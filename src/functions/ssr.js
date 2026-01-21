39e8f3b4b54137d0f3c126ed313b58dc84faeb6b
liboliang <liboliang.lbl@alibaba-inc.com>
2026-01-21 16:07:51 +0800

    opt ssr page

--- src/functions/ssr.js
+++ src/functions/ssr.js
@@ -1,10 +1,13 @@
 import { Hono } from 'hono';
+import { SSRPage } from './components/SSRPage.jsx';
 
 const ssr = new Hono();
 
 ssr.get('/:name', (c) => {
   const { name } = c.req.param();
-  return c.html(`Hello ${name} - This is SSR Page!`);
+  const timestamp = new Date().toLocaleString();
+  
+  return c.html(`<!DOCTYPE html>${SSRPage({ name, timestamp })}`);
 });
 
 export default ssr;
